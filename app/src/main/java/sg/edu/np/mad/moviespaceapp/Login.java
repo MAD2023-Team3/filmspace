511d872ae89d70ebe569d01a7551442bc21bc5f2
Your own username <Your own email>
2023-06-23 01:39:30 +0800

    amended login crashing when inputs are null

--- app/src/main/java/sg/edu/np/mad/moviespaceapp/Login.java
+++ app/src/main/java/sg/edu/np/mad/moviespaceapp/Login.java
@@ -49,6 +49,7 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        // login click button
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,35 +58,39 @@ public class Login extends AppCompatActivity {
                 email= String.valueOf(editTextEmail.getText());
                 password = String.valueOf(editTextpassword.getText());
 
-                // if the email textbox is empty
+
                 if(TextUtils.isEmpty(email)){
+                    // if the email textbox is empty
                     Toast.makeText(Login.this,"Enter email",Toast.LENGTH_SHORT).show();
-                }
-
-                // if the password textbox is empty
-                if(TextUtils.isEmpty(password)){
+                    progressBar.setVisibility(View.GONE);
+                } else if (TextUtils.isEmpty(password)) {
+                    // if the password textbox is empty
                     Toast.makeText(Login.this,"Enter password",Toast.LENGTH_SHORT).show();
-                }
+                    progressBar.setVisibility(View.GONE);
 
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "signInWithEmail:success");
-                                    Toast.makeText(Login.this, "Authentication success.", Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "signInWithEmail:failure");
-                                    Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }else{
+                    // when authenticated
+                    // store in firestore
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    progressBar.setVisibility(View.GONE);
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithEmail:success");
+                                        Toast.makeText(Login.this, "Authentication success.", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithEmail:failure");
+                                        Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                    }
                                 }
-                            }
-                        });
+                            });
+                }
             }
         });
     }
@@ -93,12 +98,14 @@ public class Login extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
+        // start: Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
             Intent intent = new Intent(getApplicationContext(),MainActivity.class);
             startActivity(intent);
             finish();
         }
+
+        // end: Check if user is signed in (non-null) and update UI accordingly.
     }
 }
