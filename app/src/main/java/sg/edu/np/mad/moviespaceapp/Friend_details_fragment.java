01ae8b7242f06056f4602bdb82ea27d038cceed1
Your own username <Your own email>
2023-08-04 11:50:26 +0800

    after unfriend go back to profile fragment

--- app/src/main/java/sg/edu/np/mad/moviespaceapp/Friend_details_fragment.java
+++ app/src/main/java/sg/edu/np/mad/moviespaceapp/Friend_details_fragment.java
@@ -130,8 +130,6 @@ public class Friend_details_fragment extends Fragment implements SendFameDialog.
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 remove_friend();
-                                // fragment transaction
-                                getFragmentManager().beginTransaction().replace(R.id.frameLayout, new Profilefragment()).commit();
                             }
                         })
                         .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -373,5 +371,6 @@ public class Friend_details_fragment extends Fragment implements SendFameDialog.
                 }
             }
         });
+        getFragmentManager().beginTransaction().replace(R.id.frameLayout, new Profilefragment()).commit();
     }
 }
